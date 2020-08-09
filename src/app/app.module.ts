89f1b6e9e2d12d6ae21a87fac696356a83deb6af
ParvinEyvazov <pervin.eyvazov1999@gmail.com>
2020-08-09 04:20:41 +0300

    landing-page-added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { MatInputModule } from "@angular/material/input";
+import { FormsModule } from "@angular/forms";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    SearchBarComponent
   ],
   imports: [
+    MatIconModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    FormsModule,
+    MatToolbarModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
