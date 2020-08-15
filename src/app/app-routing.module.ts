bc5821ebad606e77d7af93c9b78f13d69bec975c
ParvinEyvazov <pervin.eyvazov1999@gmail.com>
2020-08-16 02:09:31 +0300

    register-page-added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
 
 const routes: Routes = [
   { path: "", component: LandingPageComponent },
-  { path: "sign-in", component: SignInComponent }
+  { path: "sign-in", component: SignInComponent },
+  { path: "sign-up", component: SignUpComponent },
+  { path: "search", component: SearchPageComponent },
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,9 @@ import { FormsModule } from "@angular/forms";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
 
 
 
@@ -22,7 +25,10 @@ import { SignInComponent } from './pages/sign-in/sign-in.component';
     AppComponent,
     LandingPageComponent,
     SearchBarComponent,
-    SignInComponent
+    SignInComponent,
+    SignUpComponent,
+    SearchPageComponent,
+    UserCardComponent
   ],
   imports: [
     MatIconModule,
