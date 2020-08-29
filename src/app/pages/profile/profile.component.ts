13a769284effde5e90f83f220de9e64266196d02
ParvinEyvazov <pervin.eyvazov1999@gmail.com>
2020-08-30 00:37:20 +0300

    add new link modal updated

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -9,15 +9,89 @@ export class ProfileComponent implements OnInit {
 
   array: number[];
   edit_open: boolean = false;
+  socialMedias: socialMedias[];
 
   constructor() { }
 
   ngOnInit(): void {
     this.array = [1, 2, 3, 4, 5, 6];
+
+    this.socialMedias = [
+      {
+        name: "Instagram",
+        value: 0,
+        img: "../../../../assets/social_media_icons/instagram.svg"
+      },
+      {
+        name: "Twitter",
+        value: 1,
+        img: "../../../../assets/social_media_icons/twitter.svg"
+      },
+      {
+        name: "Linkedin",
+        value: 2,
+        img: "../../../../assets/social_media_icons/linkedin.svg"
+      },
+      {
+        name: "Facebook",
+        value: 3,
+        img: "../../../../assets/social_media_icons/facebook.svg"
+      },
+      {
+        name: "Github",
+        value: 4,
+        img: "../../../../assets/social_media_icons/github.svg"
+      },
+      {
+        name: "Whatsapp",
+        value: 5,
+        img: "../../../../assets/social_media_icons/whatsapp.svg"
+      },
+      {
+        name: "Telegram",
+        value: 6,
+        img: "../../../../assets/social_media_icons/telegram.svg"
+      },
+      {
+        name: "Discord",
+        value: 7,
+        img: "../../../../assets/social_media_icons/discord.svg"
+      },
+      {
+        name: "Medium",
+        value: 8,
+        img: "../../../../assets/social_media_icons/medium.svg"
+      },
+      {
+        name: "Reddit",
+        value: 9,
+        img: "../../../../assets/social_media_icons/reddit.svg"
+      },
+      {
+        name: "Tiktok",
+        value: 10,
+        img: "../../../../assets/social_media_icons/tiktok.svg"
+      },
+      {
+        name: "Wechat",
+        value: 11,
+        img: "../../../../assets/social_media_icons/wechat.svg"
+      },
+      {
+        name: "Youtube",
+        value: 12,
+        img: "../../../../assets/social_media_icons/youtube.svg"
+      },
+
+    ];
   }
 
   changeEditState() {
     this.edit_open = !this.edit_open;
   }
 
+}
+
+export interface socialMedias {
+  [key: string]: any;
 }
