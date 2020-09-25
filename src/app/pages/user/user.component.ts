41cf98d45a9ff423ec36a951a6470a7e4cf5f128
ParvinEyvazov <pervin.eyvazov1999@gmail.com>
2020-09-26 00:00:22 +0300

    favorites icon added

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
+  isFavorite: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  makeFavorite(){
+    this.isFavorite = !this.isFavorite;
   }
 
 }
