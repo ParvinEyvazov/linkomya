e57cf0e752ddbfb009dcb46fe5b978ddbe6014ad
ParvinEyvazov <pervin.eyvazov1999@gmail.com>
2020-12-21 01:48:16 +0300

    settings page - data get & error message and spinner added

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api-services/api.service';
+import { MessageService } from 'src/app/services/message.service';
+import { UserService } from 'src/app/services/user-services/user.service';
+import { ValidationService } from 'src/app/services/validation.service';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
+  user;
+  loading: boolean = false;
+  error: string;
 
-  constructor() {}
+  constructor(
+    private apiService: ApiService,
+    private userService: UserService,
+    private validator: ValidationService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
+    this.getUser(this.userService.getUserId());
   }
 
+  save() {
+    this.startLoading();
+    this.cleanError();
+    if (this.validator.validateFullname(this.user.fullname)) {
+      setTimeout(() => {
+        this.stopLoading();
+      }, 2000);
+      console.log('saving new data');
+    } else {
+      this.stopLoading();
+      this.showError(this.messageService.ErrorMessages.fullname_validation);
+    }
+  }
+
+  getUser(user_id) {
+    this.apiService
+      .getUser(user_id)
+      .toPromise()
+      .then((data) => {
+        if (data) {
+          this.user = data[0];
+          console.log(this.user);
+        }
+      });
+  }
+
+  startLoading() {
+    this.loading = true;
+  }
+
+  stopLoading() {
+    this.loading = false;
+  }
+
+  showError(error) {
+    this.error = error;
+  }
+
+  cleanError() {
+    this.error = '';
+  }
 }
--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -8,7 +8,7 @@ export class MessageService {
     email_validation: 'Invalid Email address',
     password_validation: 'Password must be at least 6 characters',
     password_not_same: 'Passwords are not same',
-    fullname_validation: 'Full name must be at least 1 characters',
+    fullname_validation: 'Full name must be at least 1 character',
     username_validation: 'Username with non valid characters.',
     used_username: 'This username is already taken.',
     wrong_link_type: 'This is not a valid url.',
