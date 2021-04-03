886c492b4777b528470496185f546f37fa1da38a
ParvinEyvazov <pervin.eyvazov1999@gmail.com>
2021-04-03 23:05:59 +0300

    settings page main selection part added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-// import { TokenInterceptor } from './services/token.interceptor';
+import { TokenInterceptor } from './services/api-services/interceptor';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -30,7 +30,6 @@ import { SettingsComponent } from './pages/settings/settings.component';
 import { MatSelectModule } from '@angular/material/select';
 import { HttpClientModule } from '@angular/common/http';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { TokenInterceptor } from './services/api-services/interceptor';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { PasswordRecoveryComponent } from './pages/password-recovery/password-recovery.component';
 import { LoadingComponent } from './components/loading/loading.component';
